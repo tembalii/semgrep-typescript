8d3a354e30cb9214f648ee16cbc3816b6ea176d0
tembalii <159453738+tembalii@users.noreply.github.com>
2025-05-15 18:25:58 +0100

    Create mat.py

---
+++ mat.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import urllib.request
+
+app = Flask(__name__)
+
+@app.route('/ssrf')
+def ssrf():
+    target = request.args.get('target')  
+    with urllib.request.urlopen(target) as response:  
+        return response.read()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
