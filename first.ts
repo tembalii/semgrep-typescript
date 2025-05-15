b35feb28e785d91c871200813cf45da94c0c7ff2
tembalii <159453738+tembalii@users.noreply.github.com>
2025-05-15 17:40:54 +0100

    Update first.ts

--- first.ts
+++ first.ts
@@ -1,1 +1,16 @@
+import fetch from 'node-fetch';
+import http from 'http';
 
+function greet(name: string): string {
+  return `Hello, ${name}`;
+}
+
+http.createServer((req, res) => {
+  const url = req.url?.slice(1); // risky
+  fetch(url!).then(r => r.text()).then(data => {
+    res.end(data);
+  });
+}).listen(3000);
+
+console.log("Running server...");
+console.log(greet("world"));
