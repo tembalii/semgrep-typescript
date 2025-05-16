696b8d9f5bacd03bd4ee1bb9e30c37ba1d9f5477
tembalii <159453738+tembalii@users.noreply.github.com>
2025-05-17 00:06:12 +0100

    Create 3rd.ex

---
+++ 3rd.ex
@@ -0,0 +1,19 @@
+# ruleid: sql-injection
+Ecto.Adapters.SQL.query(Repo, user_input)
+
+# ruleid: sql-injection
+Repo.query(user_input)
+
+# ruleid: sql-injection
+SQL.query!(Repo, user_input)
+
+# ruleid: sql-injection
+Repo.query!(user_input)
+
+# ruleid: sql-injection
+Ecto.Adapters.SQL.stream(Repo, user_input)
+
+# ok: sql-injection
+
+Path.expand("../deps", __DIR__)
+hubibink
